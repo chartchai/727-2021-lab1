0ebb415289eddb7d965636d518fc27715dd90b8a
Jiramed <jiramed_wi@cmu.ac.th>
2021-07-16 15:48:15 +0700

    Service by pee

--- src/main/java/th/ac/camt/automatedtest/tdd/service/ExchangeServiceImpl.java
+++ src/main/java/th/ac/camt/automatedtest/tdd/service/ExchangeServiceImpl.java
@@ -6,11 +6,16 @@ public class ExchangeServiceImpl {
         double result = 0.00;
         if(source==CurrencyEnum.USD && target==CurrencyEnum.THB){
             result = amount*32.74;
-
         }if(source==CurrencyEnum.THB && target==CurrencyEnum.EURO){
             result = amount/38.65;
         }if (source.equals(CurrencyEnum.THB) && target.equals(CurrencyEnum.USD)){
             result = amount/32.74;
+        }if (source.equals(target)){
+            result = amount;
+        }if (source.equals(CurrencyEnum.EURO) && target.equals(CurrencyEnum.USD)){
+            result = amount*1.18;
+        }if (source.equals(CurrencyEnum.USD) && target.equals(CurrencyEnum.EURO)){
+            result = amount*0.85;
         }
 
         return result;
