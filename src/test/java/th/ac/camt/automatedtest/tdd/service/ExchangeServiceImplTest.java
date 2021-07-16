c2e59fca8ebf202473339b9fcde12f87b65cf125
Chartchai <chartchai.d@cmu.ac.th>
2021-07-16 15:24:46 +0700

    bill

--- src/test/java/th/ac/camt/automatedtest/tdd/service/ExchangeServiceImplTest.java
+++ src/test/java/th/ac/camt/automatedtest/tdd/service/ExchangeServiceImplTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.Matchers.*;
 class ExchangeServiceImplTest {
 
     @Test
-    public void testExchange(){
+    public void testExchange1(){
         ExchangeServiceImpl es = new ExchangeServiceImpl();
         assertThat(es.exchange(CurrencyEnum.USD,CurrencyEnum.THB,2),
                 is(equalTo(65.48)));
@@ -21,6 +21,16 @@ class ExchangeServiceImplTest {
                 is(closeTo(0.152,0.001)));
         assertThat(es.exchange(CurrencyEnum.THB,CurrencyEnum.EURO,10),
                 is(closeTo(0.26,0.01)));
+
+    }
+    // test cases created by Bew
+    @Test
+    public void testExchange2(){
+
+    }
+    //test cases created by Pee
+    @Test
+    public void testExchange3(){
 
     }
 
