96573e3c460e778f518ba85820f549230cfbf4ad
beaw_2121 <beawglom2013@gmail.com>
2021-08-05 22:36:20 +0700

    add ServiceGrade call scores item

--- src/main/java/th/ac/camt/automatedtest/tdd/service/ServiceGrade.java
+++ src/main/java/th/ac/camt/automatedtest/tdd/service/ServiceGrade.java
@@ -3,7 +3,18 @@ package th.ac.camt.automatedtest.tdd.service;
 public class ServiceGrade {
 
     public Grade CalGrade(ScoreItem item){
-        return null;
+        int scores  = (item.score/item.maxScore)*100;
+            if (scores >= 80){
+                return Grade.A;
+            }if (scores > 70){
+                return Grade.B;
+            }if (scores > 60){
+                return Grade.C;
+            }if (scores > 50){
+                return Grade.D;
+            }else {
+                return Grade.F;
+            }
     }
 
     public Grade  TotalGrade (int... scores){
