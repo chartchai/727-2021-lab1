b753bc9ec8ca09d811ad1a9901a06a8acb1836ce
beaw_2121 <beawglom2013@gmail.com>
2021-08-05 23:16:57 +0700

    create file PropotionScoreItem
    I dont any idea

---
+++ src/main/java/th/ac/camt/automatedtest/tdd/service/PropotionScoreItem.java
@@ -0,0 +1,13 @@
+package th.ac.camt.automatedtest.tdd.service;
+
+public class PropotionScoreItem {
+    String name;
+    int score;
+    int maxScore;
+
+    public PropotionScoreItem(String name, int score, int maxScore){
+        this.name = name;
+        this.score = score;
+        this.maxScore = maxScore;
+    }
+}
